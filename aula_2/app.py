8f2d96f97aa2e23a87a6622382b54ae5deb6798e
Igor Iglesias <contato@igoriglesias.com>
2025-03-21 09:00:44 -0300

    Aula2 Variaveis

---
+++ aula_2/app.py
@@ -0,0 +1,18 @@
+
+# nome: str = "joao"
+# idade: int = 20
+# salario: float = 1000.50
+# empregado: bool = True
+
+# lista_de_nomes: list = ["joao", "maria", "pedro"]
+# pessoas: dict = {"nome": "joao", "idade": 20}
+# idades: tuple = (20, 30, 40)
+# salario: set = {1000, 2000, 3000}
+
+# NOME: str = "joao"
+
+# nome: str = input("Digite seu nome: ")
+# print(f"Seu nome é: {nome}, do tipo: {type(nome)}")
+
+idade: int = int(input("Digite sua idade: "))
+print(f"Sua idade é: {idade}, do tipo: {type(idade)}")
