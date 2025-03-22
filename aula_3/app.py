d3f9e98c14be72c27f5f9c30a1253183e4189ba5
Igor Iglesias <contato@igoriglesias.com>
2025-03-22 10:18:17 -0300

    Aula3 Condicionais

---
+++ aula_3/app.py
@@ -0,0 +1,16 @@
+idade = int(input("Digite sua idade: "))
+
+if (idade == 18):
+    print("tem 18 anos")
+
+elif idade == 19:
+    print("tem 19 anos")
+
+elif idade == 20:
+    print("tem 20 anos")
+
+elif idade >=21:
+    print("é maior de idade")
+
+else:
+    print("é menor de idade")
