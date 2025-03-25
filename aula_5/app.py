4a5be698a8bf249ace5732a76ace08347db94f32
Igor Iglesias <contato@igoriglesias.com>
2025-03-25 14:57:22 -0300

    Aula5 Loops

---
+++ aula_5/app.py
@@ -0,0 +1,15 @@
+# lista = range(1, 10)
+
+# for item in lista:
+#     print("ola")
+#     if item == 4:
+#         continue
+#     print(item)
+
+contador = 0
+
+while contador < 10:
+    contador += 1
+    if contador == 4:
+        continue
+    print(contador)
