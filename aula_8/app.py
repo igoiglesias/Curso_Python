baf19ceca8b09f530502d14e1fc11fe42a6ea03d
Igor Iglesias <contato@igoriglesias.com>
2025-05-02 08:22:59 -0300

    Aula 8 Strings

---
+++ aula_8/app.py
@@ -0,0 +1,17 @@
+texto = "  Ola mundo  "
+texto2 = " oi pra vc"
+# print(texto[0:3])
+# print(texto[-1])
+# print(texto.strip())
+# print(texto.replace("Ola", "Oi"))
+# print(texto.title())
+# print(f"texto aleatório {texto.strip()} qualquer")   
+# print("texto aleatório {} qualquer {}".format(texto.strip().upper(), texto.strip()))   
+
+# config = "postgres:{}@{}:{}"
+
+# print(config.format("123", "localhost", "5432"))
+
+print(texto.strip().split(" "))
+
+print(texto.strip() + texto2 + "rfdfdf")
